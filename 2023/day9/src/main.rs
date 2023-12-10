98d739f238d77b12b533c05f2203a2bbfec1cf78
Pablo-Arevalo-Escobar <pabloa777@gmail.com>
2023-12-09 23:49:38 -0500

    Day 9: Advent of rust

---
+++ 2023/day9/src/main.rs
@@ -0,0 +1,48 @@
+use std::io::prelude::*;
+
+fn main() {
+    // Read file input 
+    let mut file = std::fs::File::open("input.txt")
+        .expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+
+    let lines = contents.lines();
+
+    // Initialize freq map
+    let mut sum : i64 = 0;
+    for line in lines {
+        let history : Vec<i64> = line.split_whitespace()
+            .map(|s| s.parse::<i64>().unwrap())
+            .collect();
+        let mut next_value = history[0];
+        next_value -= get_history_value(history);
+        sum += next_value;
+    }
+    println!("sum {}", sum);
+}
+
+
+fn get_history_value(history : Vec<i64>) -> i64 {
+    let mut difference_vector : Vec<i64> = Vec::new();
+    let mut is_zero_vec : bool = true;
+    for i in 0..=history.len()-2 {
+        let difference = history[i+1] - history[i];
+        difference_vector.push(difference);
+        if difference != 0 {
+            is_zero_vec = false;
+        }
+    }
+
+    // Zero vector
+    let mut dif_debug: Vec<i64> = difference_vector.clone();
+    if is_zero_vec {
+        dif_debug.push(0);
+        return 0;
+    }
+
+    let first_difference : i64 =difference_vector[0];
+    let difference_increase = get_history_value(difference_vector);
+    return first_difference  - difference_increase;
+}
